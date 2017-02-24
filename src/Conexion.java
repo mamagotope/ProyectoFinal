8bee0a4fcdb74d21279cafb61f965180ee79c168
mamagotope <m4r10gm@gmail.com>
2017-02-24 18:14:07 +0100

    Creación del proyecto y añado las librerias

---
+++ src/Conexion.java
@@ -0,0 +1,28 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Conexion {
+	Connection conexion;
+	public void conectar() throws ClassNotFoundException, SQLException{
+		Class.forName("com.mysql.jdbc.Driver");
+		String url = "jdbc:mysql://31.200.243.193:51987/dbdamproject";
+		Connection con = DriverManager.getConnection(url, "usrdamproject","GgQXVfYTCu3eBsXY");
+		if(con!=null){
+			conexion=con;
+		}
+
+	}
+	  
+	public boolean comprobarlogin(String user,String pass) throws SQLException{
+		Statement consulta=conexion.createStatement();
+		ResultSet res=consulta.executeQuery("select * from usuarios where idusuarios like '"+user+"' and pass like '"+pass+"'");
+		
+		//ResultSet res=consulta.executeQuery("select * from usuarios where idusuarios like '"+user+"' and pass like '"+pass+"'");
+		return res.next();
+	}
+}
